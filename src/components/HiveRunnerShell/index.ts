d90dde4b68dbd64b99cc50e45a4538a5b002668b
TimHarris707 <tim@lendmanagement.com>
2026-06-04 09:07:38 -0700

    chore: remove unused UI stubs

--- src/components/HiveRunnerShell/index.ts
+++ src/components/HiveRunnerShell/index.ts
@@ -1,7 +1,2 @@
 export { Dock } from "./Dock";
 export { TopBar } from "./TopBar";
-export { SectionHeader } from "./SectionHeader";
-export { MetricCard } from "./MetricCard";
-export { AgentRow } from "./AgentRow";
-export { ActivityRow } from "./ActivityRow";
-export { CronRow } from "./CronRow";
